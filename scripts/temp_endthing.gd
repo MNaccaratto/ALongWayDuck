904d3aee8f2dc0d1e39b251c4d03ee190692e6fd
MNaccaratto <mnaccaratto@gmail.com>
2025-04-22 17:42:27 -0400

    comment out QB API

--- scripts/temp_endthing.gd
+++ scripts/temp_endthing.gd
@@ -5,6 +5,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/end_screen.tscn")
 
-func _ready() -> void:
-	QuackboxApi.add_leaderboard_entry("Coins", game_manager.get_score(), 1)
-	
+#func _ready() -> void:
+	#QuackboxApi.add_leaderboard_entry("Coins", game_manager.get_score(), 1)
+	#
