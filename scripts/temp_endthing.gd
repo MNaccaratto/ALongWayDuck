f0398ce0a2817908df6ba17dbfb09278b2d3cc5a
MNaccaratto <mnaccaratto@gmail.com>
2025-04-24 17:36:43 -0400

    Edit temp_endthing

--- scripts/temp_endthing.gd
+++ scripts/temp_endthing.gd
@@ -4,7 +4,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/end_screen.tscn")
-
-#func _ready() -> void:
-	#QuackboxApi.add_leaderboard_entry("Coins", game_manager.get_score(), 1)
-	#
+	QuackboxApi.add_leaderboard_entry("Coins", game_manager.get_score(), 1)
